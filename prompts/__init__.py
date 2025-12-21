cc6f3f403c756ebc1ad79e70e9391141faf5e7e1
Umesh Sharma <150319020+A-jmesh@users.noreply.github.com>
2025-12-21 20:45:39 +0530

    Update prompts package to export load_prompt and long blog prompt

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -2,7 +2,9 @@
 Prompts package for Alwrity Blog Writer
 Exports prompt loading and formatting functions
 """
+from .prompt_loader import load_prompt
 from .short_blog_prompt import get_short_blog_prompt
+from .long_blog_prompt import get_long_blog_prompt
 
-__all__ = ['get_short_blog_prompt']
+__all__ = ['load_prompt', 'get_short_blog_prompt', 'get_long_blog_prompt']
 
