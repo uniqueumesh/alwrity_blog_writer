b58477286651b087bdaba90b1f43dbb0cdd8c896
Umesh Sharma <150319020+A-jmesh@users.noreply.github.com>
2025-12-21 18:35:50 +0530

    Add prompts package __init__.py file

---
+++ prompts/__init__.py
@@ -0,0 +1,8 @@
+"""
+Prompts package for Alwrity Blog Writer
+Exports prompt loading and formatting functions
+"""
+from .short_blog_prompt import get_short_blog_prompt
+
+__all__ = ['get_short_blog_prompt']
+
